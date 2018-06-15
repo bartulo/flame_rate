cae239d0519179cee88e00f22cac4430046b07a2
bartulo <bartulo@gmail.com>
2018-06-15 16:03:06 +0200

    fichero blender

--- fire_dispersion.py
+++ fire_dispersion.py
@@ -17,8 +17,8 @@ from libtiff import TIFF
 diffusion_coefficient = 5. # m2 / s
 ambient_temperature = 310. # K
 heat_loss_time_constant = 120. # s
-velocity_x = 0.25 # m / s
-velocity_y = 0.08 # m / s
+velocity_x = 0.45 # m / s
+velocity_y = 0.18 # m / s
 ignition_temperature = 561. # K valor inicial: 561
 burn_temperature = 1400. # K
 burn_time_constant = 0.5 * 3600. # s
@@ -29,6 +29,8 @@ intercept_1 = 100. # m
 intercept_2 = 170. # m
 wood_1 = 100. # kg / m2
 wood_2 = 70. # kg / m2
+
+diagonal_factor = .707
 
 length = 650. # meters; domain extends from -length to +length
 # A grid size of 50 x 50 ist much too small to see the correct result. For a better result, set the size to 200 x 200. That computation would, however, be far too long for the Web-based development environment. You may want to run it offline.
@@ -45,7 +47,7 @@ def grid2physical(i, j):
 def wildfire():
 	temperatures_old = ambient_temperature * numpy.ones([size, size]) # K
 	data = numpy.zeros((size, size), dtype = 'uint16')
-	wood_old = 800 * numpy.ones([size, size]) # kg / m2
+	wood_old = 1000 * numpy.ones([size, size]) # kg / m2
 	
 	#~ for j in range(50, 60):
 		#~ for i in range(50, 60):
@@ -74,16 +76,16 @@ def wildfire():
 					elif i == size - 2:
 						temperatures_new[size -2 -step][i] = burn_temperature
 					else:
-						temperatures_new[j][i] = temp + h * (diffusion_coefficient / dx ** 2 * (temperatures_old[j][i+1] + temperatures_old[j][i-1] + temperatures_old[j+1][i] + temperatures_old[j-1][i] + 0.25 * (temperatures_old[j+1][i+1] + temperatures_old[j+1][i-1] + temperatures_old[j-1][i+1] + temperatures_old[j-1][i-1])- 5 * temp) - 0.5 / dx * (velocity_x * (temperatures_old[j+1][i] - temperatures_old[j-1][i]) + velocity_y * (temperatures_old[j][i+1] - temperatures_old[j][i-1]))) - (temp - ambient_temperature) / heat_loss_time_constant + heating_value * burn_rate
+						temperatures_new[j][i] = temp + h * (diffusion_coefficient / dx ** 2 * (temperatures_old[j][i+1] + temperatures_old[j][i-1] + temperatures_old[j+1][i] + temperatures_old[j-1][i] + diagonal_factor * (temperatures_old[j+1][i+1] + temperatures_old[j+1][i-1] + temperatures_old[j-1][i+1] + temperatures_old[j-1][i-1])- (4 + 4 * diagonal_factor) * temp) - 0.5 / dx * (velocity_x * (temperatures_old[j+1][i] - temperatures_old[j-1][i]) + velocity_y * (temperatures_old[j][i+1] - temperatures_old[j][i-1]))) - (temp - ambient_temperature) / heat_loss_time_constant + heating_value * burn_rate
 				elif step < 2 * size -4:
 					if j == 1: 
 						temperatures_new[j][2 * size -3 - step] = burn_temperature
 					elif i == 1:
 						temperatures_new[2 * size -3 -step][i] = burn_temperature
 					else:
-						temperatures_new[j][i] = temp + h * (diffusion_coefficient / dx ** 2 * (temperatures_old[j][i+1] + temperatures_old[j][i-1] + temperatures_old[j+1][i] + temperatures_old[j-1][i] + 0.25 * (temperatures_old[j+1][i+1] + temperatures_old[j+1][i-1] + temperatures_old[j-1][i+1] + temperatures_old[j-1][i-1]) - 5 * temp) - 0.5 / dx * (velocity_x * (temperatures_old[j+1][i] - temperatures_old[j-1][i]) + velocity_y * (temperatures_old[j][i+1] - temperatures_old[j][i-1]))) - (temp - ambient_temperature) / heat_loss_time_constant + heating_value * burn_rate
+						temperatures_new[j][i] = temp + h * (diffusion_coefficient / dx ** 2 * (temperatures_old[j][i+1] + temperatures_old[j][i-1] + temperatures_old[j+1][i] + temperatures_old[j-1][i] + diagonal_factor * (temperatures_old[j+1][i+1] + temperatures_old[j+1][i-1] + temperatures_old[j-1][i+1] + temperatures_old[j-1][i-1]) - (4 + 4 * diagonal_factor) * temp) - 0.5 / dx * (velocity_x * (temperatures_old[j+1][i] - temperatures_old[j-1][i]) + velocity_y * (temperatures_old[j][i+1] - temperatures_old[j][i-1]))) - (temp - ambient_temperature) / heat_loss_time_constant + heating_value * burn_rate
 				else:
-					temperatures_new[j][i] = temp + h * (diffusion_coefficient / dx ** 2 * (temperatures_old[j][i+1] + temperatures_old[j][i-1] + temperatures_old[j+1][i] + temperatures_old[j-1][i] + 0.25 * (temperatures_old[j+1][i+1] + temperatures_old[j+1][i-1] + temperatures_old[j-1][i+1] + temperatures_old[j-1][i-1])- 5 * temp) - 0.5 / dx * (velocity_x * (temperatures_old[j+1][i] - temperatures_old[j-1][i]) + velocity_y * (temperatures_old[j][i+1] - temperatures_old[j][i-1]))) - (temp - ambient_temperature) / heat_loss_time_constant + heating_value * burn_rate
+					temperatures_new[j][i] = temp + h * (diffusion_coefficient / dx ** 2 * (temperatures_old[j][i+1] + temperatures_old[j][i-1] + temperatures_old[j+1][i] + temperatures_old[j-1][i] + diagonal_factor * (temperatures_old[j+1][i+1] + temperatures_old[j+1][i-1] + temperatures_old[j-1][i+1] + temperatures_old[j-1][i-1])- (4 + 4 * diagonal_factor) * temp) - 0.5 / dx * (velocity_x * (temperatures_old[j+1][i] - temperatures_old[j-1][i]) + velocity_y * (temperatures_old[j][i+1] - temperatures_old[j][i-1]))) - (temp - ambient_temperature) / heat_loss_time_constant + heating_value * burn_rate
 				
 				if data[j][i] == 0:
 					if temperatures_new[j][i] > ignition_temperature:
@@ -92,7 +94,7 @@ def wildfire():
 		temperatures_old, temperatures_new = temperatures_new, temperatures_old
 		wood_old, wood_new = wood_new, wood_old
 
-	img = TIFF.open('p.tiff', mode='w')
+	img = TIFF.open('p3.tiff', mode='w')
 	img.write_image(data)
 	img.close()
 	return temperatures_old
